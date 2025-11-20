0764280e559e5e4c3cb6ef82954529c1ea0ceecb
Gerhardus Coenraad Potgieter <25999656@sun.ac.za>
2025-11-20 11:01:08 +0200

    Added sprite and some text

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,36 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
 
+    // Load a sprite to display
+    const sf::Texture texture("../src/assets/sprites/cute_image.jpg");
+    sf::Sprite sprite(texture);
+
+    // Create a graphical text to display
+    const sf::Font font("../src/assets/fonts/arial.ttf");
+    sf::Text text(font, "Hello SFML", 50);
+
+    // Start the game loop
+    while (window.isOpen()) {
+        // Process events
+        while (const std::optional event = window.pollEvent()) {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>()) window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
+        window.display();
+    }
     return 0;
 }
